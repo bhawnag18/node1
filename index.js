5bb00696eff7e05efcd05c145d077848a5e80671
bhawnag18 <bhawnagirdhar1999@gmail.com>
2022-04-13 13:36:31 +0530

    initial-commit

---
+++ index.js
@@ -0,0 +1,12 @@
+const http = require("http");
+// creating the server
+http.createServer(function(request, response){
+    
+    let obj = {
+        name:"bhawna",
+        age: 22}
+    response.write("helllo world");
+    response.write(JSON.stringify(obj));
+    // closing the server
+    response.end();
+}).listen(9000);
